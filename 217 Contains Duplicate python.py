bcf03b538a2244c17e27d32fa9d245820eff2ae9
Rambo Li <cyli071@gmail.com>
2023-05-10 10:45:07 -0700

    add a little thought

--- 217 Contains Duplicate python.py
+++ 217 Contains Duplicate python.py
@@ -8,11 +8,15 @@ class Solution:
                 s[i] = 1 # any flag can work here
         return False
 """ 
-Hashtable comprises of a hash function and an array. The hashed result(must be a number) is the index of the array. 
-Whenever you want to see if something already exist, hash it then go direct to the position. So the lookup is O(1).
+Hashtable comprises a hash function and an array. The hashed result(must be a number) is the index of the array. 
+Whenever you want to see if something already exists, hash it then go direct to the position. So the lookup is O(1).
 Python dictionary is actually a hashtable.
 """
 
 """ This is a one-liner if succinctness is what you are after
     return len(set(nums)) != len(nums)
 """
+
+""" Since this problem will give you an array of integers, we actually don't need the hash function. But then you need to 
+deal with dynamic size of the array. I tried to allocate a big array at the beginning and time ran out, so it means it has
+to be a dynamic array, whose implementation is harder than the problem itself."""
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 My solution to leetcode problems as well as my thought on them.
+
+The order of the subjects is from https://neetcode.io/roadmap. 
