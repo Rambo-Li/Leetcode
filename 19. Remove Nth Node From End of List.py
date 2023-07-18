fc143782aeb0e9bfc36507679c0d4b7841fe4c05
Rambo Li <cyli071@gmail.com>
2023-07-17 18:50:10 -0700

    37

---
+++ 19. Remove Nth Node From End of List.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        q = deque()
+        curr = head
+        while curr:
+            q.append(curr)
+            curr = curr.next
+        while q:
+            left = q.popleft()
+            right = q.pop() if q else None
+            temp = left.next
+            left.next = right
+            if right:
+                right.next = temp if right != temp else None
