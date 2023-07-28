171e4acb76ce8f1a4fa385c0a474b8d994300308
Rambo Li <cyli071@gmail.com>
2023-07-28 15:45:51 -0700

    78

---
+++ 91. Decode Ways.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s[0] == '0':
+            return 0
+        res = [1]*(len(s)+1)
+        for i in range(1, len(s)):
+            if int(s[i-1:i+1]) <= 26 and s[i]!= '0' and s[i-1]!='0':
+                res[i+1] = res[i] + res[i-1]            
+            elif (int(s[i-1:i+1]) > 26 or s[i-1]== '0') and s[i]== '0' :
+                return 0
+            elif s[i-1] == '0' or int(s[i-1:i+1]) > 26:
+                res[i+1] = res[i]
+            else:
+                res[i+1] = res[i-1]
+        return res[-1]
