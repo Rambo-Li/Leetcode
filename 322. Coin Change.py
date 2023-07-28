35d2b403922c5e31ec2a123cf8cfc3bf3d5ab121
Rambo Li <cyli071@gmail.com>
2023-07-28 16:36:09 -0700

    79

---
+++ 322. Coin Change.py
@@ -0,0 +1,10 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        res = [math.inf] * (amount+1)
+        res[0] = 0
+        for i in range(amount+1):
+            for c in coins:
+                if i-c>=0:
+                    res[i] = min(res[i], res[i-c]+1)
+        
+        return -1 if res[-1]==math.inf else res[-1]
