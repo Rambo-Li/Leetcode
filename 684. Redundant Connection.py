7290d9650c9b6c0da834acd913d7bb634811c7a6
Rambo Li <cyli071@gmail.com>
2023-08-04 15:13:07 -0700

    99

---
+++ 684. Redundant Connection.py
@@ -0,0 +1,19 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        union = [i for i in range(len(edges)+1)]
+        def root(node):
+            p = node
+            while union[p] != p:
+                p = union[p]
+            union[node] = p
+            return p
+        def connect(n1, n2):
+            p1 = root(n1)
+            p2 = root(n2)
+            if p1 == p2:
+                return False
+            else:
+                union[p1] = p2
+        for n1, n2 in edges:
+            if connect(n1, n2) == False:
+                return [n1,n2]
