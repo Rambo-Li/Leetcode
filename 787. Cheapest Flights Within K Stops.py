bdd72a1706a67c93d91a0e5c9fa59352c2b6918c
Rambo Li <cyli071@gmail.com>
2023-12-25 09:52:17 -0800

    117

--- 787. Cheapest Flights Within K Stops.py
+++ 787. Cheapest Flights Within K Stops.py
@@ -23,8 +23,10 @@ class Solution:
             k -= 1
         return distance[dst] if distance[dst] != inf else -1
     
-# Concept: BFS within K steps. Or Bellman Ford for shortest path.
-# DS: BFS uses a queue, but K steps requires seperation of layers so an extra count variable or temp array is needed.
-#     A array to keep the result. The array might be updated more than once per iteration, if the from and to are both updated, 
-#     then the distance is actually not achievable. So a temp array is needed to make sure the from is always from last iteration.
-      
+'''
+Concept: BFS within K steps. Or Bellman Ford for shortest path.
+DS: BFS uses a queue, but K steps requires seperation of layers so an extra count variable or temp array is needed.
+    A array to keep the result. The array might be updated more than once per iteration, if the from and to are both updated, 
+    then the distance is actually not achievable. So a temp array is needed to make sure the from is always from last iteration.
+Algo: BFS, only put distance-changed nodes on the queue. Use last iteration 'from' and this iteration 'to' to determine if distance changed.
+'''      
