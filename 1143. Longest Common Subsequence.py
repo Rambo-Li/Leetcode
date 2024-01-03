b6922d25851765b8f1ed9d9582e4607dda144965
Rambo Li <cyli071@gmail.com>
2024-01-03 14:51:30 -0800

    134

---
+++ 1143. Longest Common Subsequence.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m = len(text1)
+        n = len(text2)
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        return dp[-1][-1]
+
+"""
+Concept: minimum steps or distance between two strings
+DS: one matrix
+Algo: how to formulate the current steps given all former steps, base on two senarios: current character equal or not
+"""
